22046762a9ddf4f46a1af6888376db428f674983
terx7 <erkileemet10@gmail.com>
2020-09-08 11:46:59 +0300

    stuff

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once'../config'
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$stmt = $pdo->prepare('SELECT * FROM books');
+$stmt->execute();
+$aBooks = $stmt->fetchAll();
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php
+    foreach ($aBooks as $book) {
+        echo"<li>" .$book[title] ."</li>";
+    }
+    ?>
+    </ul>
+</body>
+</html>
